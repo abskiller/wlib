4c31da5361ff2c990393aa8e660a17e610d9393d
abskiller <imriver@outlook.com>
2024-10-23 14:30:24 +0800

    First commit Changes to be committed: 	new file:   include/bytearray.h 	new file:   include/codec.h 	new file:   include/datetime.h 	new file:   include/dir.h 	new file:   include/file.h 	new file:   include/logger.h 	new file:   include/wlib.h 	new file:   include/xorstr.h 	new file:   src/bytearray.cpp 	new file:   src/codec.cpp 	new file:   src/datetime.cpp 	new file:   src/dir.cpp 	new file:   src/example/xorstr/CMakeLists.txt 	new file:   src/example/xorstr/xorstr.cpp 	new file:   src/file.cpp 	new file:   src/logger.cpp

---
+++ include/wlib.h
@@ -0,0 +1,11 @@
+#ifndef _WLIB_H_
+#define _WLIB_H_
+
+
+#define INDEXTOSIZE(value) value + 1
+#define SIZETOINDEX(value) value - 1
+
+
+
+
+#endif
---
+++ src/example/xorstr/xorstr.cpp
@@ -0,0 +1,9 @@
+#include "xorstr.h"
+#include <iostream>
+int main()
+{
+    std::cout<< xorstr("Hello World") << std::endl;
+
+    return 0;
+
+}
